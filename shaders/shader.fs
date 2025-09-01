bee2787c4a7d3072a40bec621928715e327b5634
Peter Vu <vutrunghieu04082008@gmail.com>
2025-09-02 01:37:12 +0300

    Update fragment shader to complete Phong lighting

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -14,16 +14,25 @@ vec4 FragColor;
 uniform vec3 obColor;
 uniform vec3 liColor;
 uniform vec3 lightPos;
+uniform vec3 viewPos;
 
 void main()
 {
     
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * liColor;
+    
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * liColor;
-    vec3 res = (ambient + diffuse)*obColor;
+    
+    float specStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir),0.0),64);
+    vec3 specular = specStrength * spec * liColor;
+
+    vec3 res = (ambient + diffuse + specular)*obColor;
     FragColor = vec4(res,1.0);
 }
